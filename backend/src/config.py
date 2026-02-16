686b4e3d092aa8395e80b885a4633e1b91508c72
Ростислав <rostislav.rostik.sh@gmail.com>
2026-02-16 23:42:51 +0300

    Изменение конфига. Добавление jwt токенов

--- backend/src/config.py
+++ backend/src/config.py
@@ -8,9 +8,22 @@ class Settings(BaseSettings):
     DB_PASSWORD: str
     DB_NAME: str
 
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int
+
     @property
     def database_url(self) -> str:
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    @property
+    def auth_jwt_secret_key(self) -> str:
+        return self.JWT_SECRET_KEY
+
+    @property
+    def auth_jwt_algorithm(self) -> str:
+        return self.JWT_ALGORITHM
 
     model_config = SettingsConfigDict(env_file=".env")
 
