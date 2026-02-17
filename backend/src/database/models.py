d395c0048316cb0e07b8b29afbaf7fe99a59b4b9
Ростислав <rostislav.rostik.sh@gmail.com>
2026-02-17 21:49:53 +0300

    Изменение модели базы данных

--- backend/src/database/models.py
+++ backend/src/database/models.py
@@ -1,8 +1,8 @@
 import datetime
 from typing import Annotated
 
-from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import text
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import text, String, ForeignKey, Boolean
 
 from .config_db import Base
 
@@ -18,5 +18,25 @@ class Users(Base):
     __tablename__ = 'users'
 
     id: Mapped[int_pk]
-    username: Mapped[str]
-    email: Mapped[str]
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    password_hash: Mapped[str] # использует хеш пароля
+    created_at: Mapped[created_at]
+    updated_at: Mapped[updated_at]
+
+    refresh_tokens: Mapped[str]
+
+class RefreshToken(Base):
+    __tablename__ = 'refresh_tokens'
+
+    id = Mapped[int_pk]
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'), primary_key=True)
+    # Хэш refresh токена (opaque строка); используется для валидации при ротации.
+    token_hash: Mapped[str] = mapped_column(String(128), unique=True)
+    # Дата/время истечения refresh токена
+    expires_at: Mapped[datetime.datetime] = mapped_column(server_default=text("TIMEZONE('utc', now())"))
+    # Флаг отзыва: позволяет сделать токен недействительным без удаления.
+    revoked: Mapped[bool] = mapped_column(Boolean, default=False)
+    # Время создания refresh токена
+    created_at: Mapped[created_at]
+
+    user: Mapped[Users] = relationship(back_populates="refresh_tokens")
