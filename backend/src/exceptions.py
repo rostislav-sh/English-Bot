a877f93f1ec20f1d1c96b47d2d98ab66449ee77f
Ростислав <rostislav.rostik.sh@gmail.com>
2026-02-21 15:47:50 +0300

    Добавление описания для ошибок приложения. Теперь при их вызове можно сразу получать сообщение об ошибке.

--- backend/src/exceptions.py
+++ backend/src/exceptions.py
@@ -1,10 +1,18 @@
 class AppError(Exception):
     """Базовая ошибка приложения. От нее наследуются все остальные."""
 
+    def __init__(self, message: str = "Произошла непредвиденная ошибка.") -> None:
+        super().__init__(message)
 
 class UserAlreadyExistsError(AppError):
     """Пользователь уже существует."""
 
+    def __init__(self, message: str = "Пользователь с таким email уже существует"):
+        super().__init__(message)
+
 
 class InvalidCredentialsError(AppError):
     """Неверный логин или пароль"""
+
+    def __init__(self, message: str = "Неверный email или пароль"):
+        super().__init__(message)
