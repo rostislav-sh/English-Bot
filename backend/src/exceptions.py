2133985ed655420c1c6d471c7f2e29650a3030e7
Ростислав <rostislav.rostik.sh@gmail.com>
2026-02-21 17:49:30 +0300

    Убрал дублирование кода. Теперь кастомное сообщение об ошибке задается только в AppError.

--- backend/src/exceptions.py
+++ backend/src/exceptions.py
@@ -1,18 +1,32 @@
 class AppError(Exception):
     """Базовая ошибка приложения. От нее наследуются все остальные."""
 
-    def __init__(self, message: str = "Произошла непредвиденная ошибка.") -> None:
-        super().__init__(message)
+    message: str = "Произошла непредвиденная ошибка."
+
+    def __init__(self, message: str | None = None) -> None:
+        super().__init__(message or self.message)
+
 
 class UserAlreadyExistsError(AppError):
     """Пользователь уже существует."""
-
-    def __init__(self, message: str = "Пользователь с таким email уже существует"):
-        super().__init__(message)
+    message = "Пользователь с таким email уже существует."
 
 
 class InvalidCredentialsError(AppError):
-    """Неверный логин или пароль"""
+    """Неверный логин или пароль."""
+    message = "Неверный email или пароль."
 
-    def __init__(self, message: str = "Неверный email или пароль"):
-        super().__init__(message)
+
+class RefreshTokenNotFoundError(AppError):
+    """Refresh токен не найден или отозван."""
+    message = "Refresh токен не найден или отозван."
+
+
+class RefreshTokenLifetimeExpiredError(AppError):
+    """Время жизни refresh токена истекло."""
+    message = "Время жизни refresh токена истекло."
+
+
+class UserNotFoundError(AppError):
+    """Пользователь не найден."""
+    message = "Пользователь не найден."
