ea2327704db2ae59e814da6d2a9dd00c846b982f
Ростислав <rostislav.rostik.sh@gmail.com>
2026-04-15 09:36:21 +0300

    Исправление импортов.

--- backend/src/application/interfaces/unitofwork.py
+++ backend/src/application/interfaces/unitofwork.py
@@ -2,8 +2,7 @@
 
 from abc import ABC, abstractmethod
 
-from src.interfaces.repositories.auth_uow import IRefreshTokenRepository
-from src.interfaces.repositories.user_uow import IUserRepository
+from src.application.interfaces.repositories import IUserRepository, IRefreshTokenRepository
 
 
 class IUnitOfWork(ABC):
@@ -20,22 +19,22 @@ class IUnitOfWork(ABC):
         ...
 
     @abstractmethod
-    async def __aenter__(self):
+    async def __aenter__(self) -> "IUnitOfWork":
         """Открывает сессию и инициализирует репозитории."""
         ...
 
     @abstractmethod
-    async def __aexit__(self, exc_type, exc_val, exc_tb):
+    async def __aexit__(self, exc_type, exc_val, exc_tb) -> None:
         """Откатывает при ошибке и закрывает сессию."""
         ...
 
     @abstractmethod
-    async def commit(self):
+    async def commit(self) -> None:
         """Фиксирует транзакцию."""
         ...
 
     @abstractmethod
-    async def rollback(self):
+    async def rollback(self) -> None:
         """Откатывает транзакцию."""
         ...
 
