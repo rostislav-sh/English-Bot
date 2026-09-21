94cd5c2e243af8c16835f379a270a7d1cdd676c3
Ростислав <rostislav.rostik.sh@gmail.com>
2026-09-22 01:03:24 +0300

    Мок работы фронта

--- backend/src/application/services/quiz/__init__.py
+++ backend/src/application/services/quiz/__init__.py
@@ -1,5 +1,6 @@
 """Сервисы для квизов."""
 
 from .topic_service import TopicService
+from .mocks import MockTestService, MockAttemptService
 
-__all__ = ["TopicService"]
+__all__ = ["TopicService", "MockTestService", "MockAttemptService"]
